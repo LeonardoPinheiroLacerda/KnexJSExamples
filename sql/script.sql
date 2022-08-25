3db4113f97605fee934f8009d69489c7a616967e
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2022-08-24 22:12:31 -0300

    Adding table to the script.sql file

--- sql/script.sql
+++ sql/script.sql
@@ -1,5 +1,17 @@
+drop table if exists studios;
+drop table if exists games;
+
 create table games(
 	id serial primary key,
 	name varchar(120),
 	price float not null
 );
+
+create table studios(
+	id serial primary key,
+	name varchar(50),
+	game_id integer,
+	foreign key (game_id) references games(id)
+		on update cascade
+		on delete cascade
+)
