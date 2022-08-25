a2b855ca583b3ab4164322b12ae0d9c0bf49abff
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2022-08-25 09:52:07 -0300

    games_studios table created

--- sql/script.sql
+++ sql/script.sql
@@ -1,3 +1,4 @@
+drop table if exists games_studios;
 drop table if exists studios;
 drop table if exists games;
 
@@ -9,9 +10,17 @@ create table games(
 
 create table studios(
 	id serial primary key,
-	name varchar(50),
-	game_id integer,
+	name varchar(50)
+);
+
+create table games_studios(
+	game_id int,
+	studio_id int,
 	foreign key (game_id) references games(id)
 		on update cascade
-		on delete cascade
-)
+		on delete cascade,
+	foreign key (studio_id) references studios(id)
+		on update cascade
+		on delete cascade,
+	primary key(game_id, studio_id)
+);
